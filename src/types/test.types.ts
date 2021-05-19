b58288da9c27aa16db9f6d888bcf0969ce95fc9c
sampaiorafael <rafa021dev@gmail.com>
2021-05-19 15:33:59 -0300

    refac: underline to cammelCase

--- src/types/test.types.ts
+++ src/types/test.types.ts
@@ -1,16 +1,37 @@
+// Request Types
+export interface requestTestInputs {
+    title: string,
+    questions: Array<requestTestQuestionInput>
+}
+
+export interface requestTestQuestionInput {
+    title: string,
+    description?: string,
+    orderPosition: number,
+    choices?: Array<requestQuestionChoiceInput>
+}
+
+export interface requestQuestionChoiceInput {
+    text: string,
+    isCorrect: number
+}
+
+// SGBD Types
+
 export interface createTestInputs {
     title: string,
-    questions: Array<createTestQuestionInput>
 }
 
 export interface createTestQuestionInput {
     title: string,
     description?: string,
     orderPosition: number,
-    choices: Array<createQuestionChoiceInput>
+    testId: number
 }
 
 export interface createQuestionChoiceInput {
     text: string,
-    isCorrect: number
+    isCorrect: number,
+    questionId: number
 }
+
