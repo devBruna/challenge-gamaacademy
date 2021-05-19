8c3872b21def931a454a71df3b4d6d7c662f44e0
sampaiorafael <rafa021dev@gmail.com>
2021-05-19 18:58:57 -0300

    refac: status route response obj

--- src/controllers/Status.controller.ts
+++ src/controllers/Status.controller.ts
@@ -6,12 +6,15 @@ class StatusController {
 
     public async status (req: Request, res: Response, next: NextFunction): Promise<Response> {
         return res.status(200).json({
-            hostname: req.hostname,
-            path: req.path,
-            method: req.method,
-            ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
-            date: new Date(),
-            uptime: (uptime() / 60) / 60
+            status: 'success',
+            data: {
+                hostname: req.hostname,
+                path: req.path,
+                method: req.method,
+                ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+                date: new Date(),
+                uptime: (uptime() / 60) / 60
+            }
         })
     }
 
